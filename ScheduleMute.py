8e71639415b56336d3051aba4e0274845f3b8d50
Barel Elbaz <barel.elbaz@harmonicinc.com>
2023-01-15 15:54:45 +0200

    Move script to oop

--- ScheduleMute.py
+++ ScheduleMute.py
@@ -3,58 +3,58 @@ from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 import time
 import datetime
 
+class VolumeControl:
 
+    def __init__(self):
+        self.muted = False
 
-def is_time_between(begin_time, end_time, check_time=None):
-    # If check time is not given, default to current local time
-    check_time = check_time or datetime.datetime.now().time()
-    if begin_time < end_time:
-        return check_time >= begin_time and check_time <= end_time
-    else: # crosses midnight
-        return check_time >= begin_time or check_time <= end_time
+    def is_time_between(self, begin_time, end_time, check_time=None):
+        # If check time is not given, default to current local time
+        check_time = check_time or datetime.datetime.now().time()
+        if begin_time < end_time:
+            return check_time >= begin_time and check_time <= end_time
+        else: # crosses midnight
+            return check_time >= begin_time or check_time <= end_time
 
-def is_time_bigger(compareT, check_time=None):
-    check_time = check_time or datetime.datetime.now().time()
-    return compareT < check_time
+    def is_time_bigger(self, compareT, check_time=None):
+        check_time = check_time or datetime.datetime.now().time()
+        return compareT < check_time
 
-def set_volume_muted(muted):
-    sessions = AudioUtilities.GetAllSessions()
-    for session in sessions:
-        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-        #print("volume.GetMasterVolume(): %s" % volume.GetMasterVolume())
-        #volume.SetMasterVolume(0.6, None) # change sessions volume
-        if muted:
-            volume.SetMute(1, None)#mute/unmute sessions
-        else:
-            volume.SetMute(0, None) 
-
-def get_today():
-    now = datetime.datetime.now()
-    return now.strftime("%A")
-
-def main():
-    while(True):
-        today = get_today()
-        muted = False
-        if today == "Friday":
-            if is_time_bigger(datetime.time(16,30)):
-                muted = True
+    def set_volume_muted(self, muted):
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+            if muted:
+                volume.SetMute(1, None)#mute/unmute sessions
             else:
-                muted = False
-        elif today == "Saturday":
-            if is_time_bigger(datetime.time(21,00)):
-                muted = False
+                volume.SetMute(0, None) 
+
+    def get_today(self):
+        now = datetime.datetime.now()
+        return now.strftime("%A")
+
+    def main(self):
+        while(True):
+            today = self.get_today()
+            self.muted = False
+            if today == "Friday":
+                if self.is_time_bigger(datetime.time(16,30)):
+                    self.muted = True
+                else:
+                    self.muted = False
+            elif today == "Saturday":
+                if self.is_time_bigger(datetime.time(21,00)):
+                    self.muted = False
+                else:
+                    self.muted = True   
+
+            if self.is_time_between(datetime.time(7,30), datetime.time(21,00)):
+                self.set_volume_muted(self.muted)
             else:
-                muted = True   
-                
-        if is_time_between(datetime.time(7,30), datetime.time(21,00)):
-            set_volume_muted(muted)
-        else:
-            set_volume_muted(True)
+                self.set_volume_muted(True)
 
-        time.sleep(300)#sleep for 5 min
-
-
+            time.sleep(300)#sleep for 5 min
 
 if __name__ == "__main__":
-    main()
+    vol_control = VolumeControl()
+    vol_control.main()
